de3ceef883ba49af2bb38c125fc8bca02e6245fe
Stefan Georgesco <stefan.georgesco@yahoo.fr>
2023-04-07 17:10:30 +0200

    Finished branch

--- src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
+++ src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitializer.java
@@ -1,6 +1,8 @@
 package guru.springframework.sdjpaintro.bootstrap;
 
+import guru.springframework.sdjpaintro.domain.AuthorUuid;
 import guru.springframework.sdjpaintro.domain.Book;
+import guru.springframework.sdjpaintro.repositories.AuthorUuidRepository;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
@@ -14,25 +16,32 @@ import org.springframework.stereotype.Component;
 public class DataInitializer implements CommandLineRunner {
 
     private final BookRepository bookRepository;
+    private final AuthorUuidRepository authorUuidRepository;
 
-    public DataInitializer(BookRepository bookRepository) {
+    public DataInitializer(BookRepository bookRepository, AuthorUuidRepository authorUuidRepository) {
         this.bookRepository = bookRepository;
+        this.authorUuidRepository = authorUuidRepository;
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         bookRepository.deleteAll();
 
         Book bookDDD = new Book("Domain Driven Design", "123", "RandomHouse", null);
-        Book savedDDD = bookRepository.save(bookDDD);
+        bookRepository.save(bookDDD);
 
         Book bookSIA = new Book("Spring In Action", "234234", "Oriely", null);
-        Book savedSIA = bookRepository.save(bookSIA);
+        bookRepository.save(bookSIA);
 
         bookRepository.findAll().forEach(book -> {
             System.out.println("Book Id: " + book.getId());
             System.out.println("Book Title: " + book.getTitle());
         });
 
+        AuthorUuid authorUuid = new AuthorUuid();
+        authorUuid.setFirstName("Joe");
+        authorUuid.setLastName("Buck");
+        AuthorUuid savedAuthor = authorUuidRepository.save(authorUuid);
+        System.out.println("Saved Author UUID: " + savedAuthor.getId() );
     }
 }
--- src/main/resources/db/migration/common/V5__author_uuid.sql
+++ src/main/resources/db/migration/common/V5__author_uuid.sql
@@ -1,8 +1,6 @@
-drop table if exists author;
-
-create table author
+create table author_uuid
 (
-    id         bigint not null,
+    id         varchar(36) not null,
     first_name varchar(255),
     last_name varchar(255),
     primary key (id)
