9949a345f5189c2ef8ec2ad4cf72b89e8175fad9
Stefan Georgesco <stefan.georgesco@yahoo.fr>
2023-04-07 19:22:33 +0200

    Finished branch

--- src/test/java/guru/springframework/sdjpaintro/MySQLIntegrationTest.java
+++ src/test/java/guru/springframework/sdjpaintro/MySQLIntegrationTest.java
@@ -3,11 +3,9 @@ package guru.springframework.sdjpaintro;
 import guru.springframework.sdjpaintro.domain.AuthorUuid;
 import guru.springframework.sdjpaintro.domain.BookNatural;
 import guru.springframework.sdjpaintro.domain.BookUuid;
-import guru.springframework.sdjpaintro.repositories.AuthorUuidRepository;
-import guru.springframework.sdjpaintro.repositories.BookNaturalRepository;
-import guru.springframework.sdjpaintro.repositories.BookRepository;
-import guru.springframework.sdjpaintro.repositories.BookUuidRepository;
-import org.junit.jupiter.api.Order;
+import guru.springframework.sdjpaintro.domain.composite.AuthorComposite;
+import guru.springframework.sdjpaintro.domain.composite.NameId;
+import guru.springframework.sdjpaintro.repositories.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -38,6 +36,24 @@ public class MySQLIntegrationTest {
     @Autowired
     BookNaturalRepository bookNaturalRepository;
 
+    @Autowired
+    AuthorCompositeRepository authorCompositeRepository;
+
+    @Test
+    void authorCompositeTest() {
+        NameId nameId = new NameId("John", "T");
+        AuthorComposite authorComposite = new AuthorComposite();
+        authorComposite.setFirstName(nameId.getFirstName());
+        authorComposite.setLastName(nameId.getLastName());
+        authorComposite.setCountry("US");
+
+        AuthorComposite saved = authorCompositeRepository.save(authorComposite);
+        assertThat(saved).isNotNull();
+
+        AuthorComposite fetched = authorCompositeRepository.getById(nameId);
+        assertThat(fetched).isNotNull();
+    }
+
     @Test
     void bookNaturalTest() {
         BookNatural bookNatural = new BookNatural();
@@ -52,7 +68,7 @@ public class MySQLIntegrationTest {
     void testBookUuid() {
         BookUuid bookUuid = bookUuidRepository.save(new BookUuid());
         assertThat(bookUuid).isNotNull();
-        assertThat(bookUuid.getId());
+        assertThat(bookUuid.getId()).isNotNull();
 
         BookUuid fetched = bookUuidRepository.getById(bookUuid.getId());
         assertThat(fetched).isNotNull();
@@ -73,12 +89,6 @@ public class MySQLIntegrationTest {
         long countBefore = bookRepository.count();
         assertThat(countBefore).isEqualTo(2);
     }
-
-
-
-
-
-
 }
 
 
